c56b14f9efe2b73e49ac0f9fba5d90bb3fe2ff64
wl02227976 <b04201043@ntu.edu.tw>
2021-11-04 14:57:49 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,24 @@
+#number of classes
+NUM_CLASSES = 200
+
+#batch_size
+BATCH_SIZE = 20
+
+#number of epochs
+NUM_EPOCHS= 50
+
+#pretrained_model
+PRETRAINED_MODEL = ''
+
+#the path to save model
+##TRAINED_MODEL = 'result/result.pth'
+
+MODEL_SAVED_DIR = 'result/'
+
+#数据集的存放位置
+TRAIN_DATASET_DIR = 'dataset/train_data'
+VALID_DATASET_DIR = 'dataset/val_data'
+
+
+
+
